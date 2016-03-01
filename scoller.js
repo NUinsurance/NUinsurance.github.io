1df03488800fbd6e0cae15f67d08ddbc20ecd065
Yi Zhang <yi.zhang.abc@gmail.com>
2016-02-29 19:52:08 -0600

    Index Update

---
+++ scoller.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(function(){
+	(function($){
+	    $(window).load(function(){
+
+	        $("#my-thumbs-list").mThumbnailScroller({
+	          axis:"x"
+	        });
+	    });
+	})(jQuery);
+});
