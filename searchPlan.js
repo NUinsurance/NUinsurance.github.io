6a81ecde6dc57e56df5338341fadcafc976432c8
Yi Zhang <yi.zhang.abc@gmail.com>
2016-03-09 14:03:42 -0600

    Search Plan updates

---
+++ searchPlan.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $('#example').DataTable( {
+        initComplete: function () {
+            this.api().columns().every( function () {
+                var column = this;
+                var select = $('<select><option value=""></option></select>')
+                    .appendTo( $(column.footer()).empty() )
+                    .on( 'change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+
+                        column
+                            .search( val ? '^'+val+'$' : '', true, false )
+                            .draw();
+                    } );
+
+                column.data().unique().sort().each( function ( d, j ) {
+                    select.append( '<option value="'+d+'">'+d+'</option>' )
+                } );
+            } );
+        },"ajax": "data.json"
+    } );
+
+    $('#submitForm').submit(function () {
+        if(document.getElementById("submitForm").checkValidity()){
+            $("#QuestionForm").fadeOut();
+            $("#searching").fadeIn();
+            $("#searching").delay(3000).fadeOut();
+            $("#planTable").delay(3000).fadeIn();
+        }
+        return false;
+    });
+} );
